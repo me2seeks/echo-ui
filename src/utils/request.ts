153a56a537c7653c20ac7f75971ef4923295adee
me2seeks <z3653934@163.com>
2024-09-04 20:37:19 +0800

    feat: 添加请求拦截

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,9 +1,21 @@
+import { useUserStore } from '@/store/user'
 import axios from 'axios'
 
 const service = axios.create({
   baseURL: 'http://10.2.12.14:32655',
   // baseURL: 'http://localhost:1000',
   timeout: 5000,
+})
+
+service.interceptors.request.use((config) => {
+  const userStore = useUserStore()
+  const headers = new axios.AxiosHeaders(config.headers)
+
+  headers.set('Content-Type', 'application/json')
+  headers.set('Authorization', `Bearer ${userStore.token}`)
+  config.headers = headers
+  console.log(config.headers)
+  return config
 })
 
 service.interceptors.response.use(
