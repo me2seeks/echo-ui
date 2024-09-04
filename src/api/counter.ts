3dfa4677c7e3805fb76abe1c0526d48a065e5014
me2seeks <z3653934@163.com>
2024-09-04 21:49:13 +0800

    feat: 添加counter api封装

---
+++ src/api/counter.ts
@@ -0,0 +1,35 @@
+import service from '@/utils/request'
+import type { GetContentCounterResp, GetUserCounterResp } from '@/types/counter'
+
+//@Summary get comment counter
+//@Produce application/json
+//@Param id path string true "Comment ID"
+//@Router /counter/comment/{id} [get]
+export const getCommentCounter = (id: number): Promise<GetContentCounterResp> => {
+  return service({
+    url: `/counter/comment/${id}`,
+    method: 'get',
+  })
+}
+
+//@Summary get feed counter
+//@Produce application/json
+//@Param id path string true "Feed ID"
+//@Router /counter/feed/{id} [get]
+export const getFeedCounter = (id: number): Promise<GetContentCounterResp> => {
+  return service({
+    url: `/counter/feed/${id}`,
+    method: 'get',
+  })
+}
+
+//@Summary get user counter
+//@Produce application/json
+//@Param id path string true "User ID"
+//@Router /counter/user/{id} [get]
+export const getUserCounter = (id: number): Promise<GetUserCounterResp> => {
+  return service({
+    url: `/counter/user/${id}`,
+    method: 'get',
+  })
+}
---
+++ src/types/counter.ts
@@ -0,0 +1,11 @@
+export interface GetContentCounterResp {
+  CommentCount: number
+  LikeCount: number
+  ViewCount: number
+}
+
+export interface GetUserCounterResp {
+  FollowingCount: number
+  FollowerCount: number
+  FeedCount: number
+}
