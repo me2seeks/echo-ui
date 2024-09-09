f5d103b615bff09b5b586657c8f4e3ec62098bbd
me2seeks <z3653934@163.com>
2024-09-09 21:21:39 +0800

    feat: followBtn 获取父组件的userID

--- src/components/FollowBtn.vue
+++ src/components/FollowBtn.vue
@@ -1,6 +1,12 @@
 <script setup lang="ts">
+  const props = defineProps({
+    userId: {
+      type: String,
+      required: true,
+    },
+  })
   function follow() {
-    console.log('Followed')
+    console.log('Followed', props.userId)
   }
 </script>
 
