493dca028b0e234cce256aaafd0e2bb06c7ce226
me2seeks <z3653934@163.com>
2024-09-09 21:21:12 +0800

    feat: 在index 中 创建feed

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,10 +1,19 @@
 <script setup lang="ts">
   import router from '@/router'
   import { useUserStore } from '@/store/user'
+  import { TransitionRoot, TransitionChild, Dialog, DialogPanel } from '@headlessui/vue'
   const userStore = useUserStore()
   const Logout = () => {
     userStore.LoginOut()
     router.push('/login')
+  }
+  const isOpen = ref(false)
+
+  function closeModal() {
+    isOpen.value = false
+  }
+  function openModal() {
+    isOpen.value = true
   }
 </script>
 <template>
@@ -135,8 +144,46 @@
               </li>
             </ul>
             <div class="w-56 h-14 lg:inline md:hidden">
-              <button class="btn btn-block rounded-3xl items-center bg-blue-600">Post</button>
+              <button class="btn btn-block rounded-3xl items-center bg-blue-600" type="button" @click="openModal">
+                Post
+              </button>
             </div>
+
+            <TransitionRoot appear :show="isOpen" as="template">
+              <Dialog class="relative z-10" as="div" @close="closeModal">
+                <TransitionChild
+                  as="template"
+                  enter="duration-300 ease-out"
+                  enter-from="opacity-0"
+                  enter-to="opacity-100"
+                  leave="duration-200 ease-in"
+                  leave-from="opacity-100"
+                  leave-to="opacity-0"
+                >
+                  <div class="fixed inset-0 bg-black/25" />
+                </TransitionChild>
+
+                <div class="fixed inset-0 overflow-y-auto">
+                  <div class="flex min-h-full items-center justify-center p-4 text-center">
+                    <TransitionChild
+                      as="template"
+                      enter="duration-300 ease-out"
+                      enter-from="opacity-0 scale-95"
+                      enter-to="opacity-100 scale-100"
+                      leave="duration-200 ease-in"
+                      leave-from="opacity-100 scale-100"
+                      leave-to="opacity-0 scale-95"
+                    >
+                      <DialogPanel
+                        class="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all"
+                      >
+                        <contentBox />
+                      </DialogPanel>
+                    </TransitionChild>
+                  </div>
+                </div>
+              </Dialog>
+            </TransitionRoot>
           </div>
         </div>
         <div class="w-56 px-2 h-16 mt-auto dropdown dropdown-top md:hidden lg:inline">
